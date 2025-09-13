75af9cd66e1d3754759ce23f4478a51e60ceee88
Griffo81 <mark.a.griffiths81@gmail.com>
2025-09-13 10:59:01 +0100

    Task 25 - Add hello_world.py file

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+''' Task 25 - Version Control and Git Basics - Hello World '''
+
+# Print "Hello, World!" to the console
+# print("Hello, World!")
+# Update the message to "Git is awesome!"
+print("Git is awesome!")
